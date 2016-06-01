a1843abab4a8ebaefd977d283f8a8481bc92d7f7
Zak Weston <zweston@MHC.local>
2016-06-01 10:44:48 -0400

    Used rspec to fix issues with current_date_and_time, bootstrap_alert and image_tag helpers

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -1,22 +1,29 @@
 module MethodsPracticeHelpers
 
   def image_tag(source, alternate_text)
-    # Generate an image tag with the given source and the classes
-    # "img-responsive" and "img-thumbnail". The image should also have an alt
+    # Generates an image tag with the given source and the classes
+    # "img-responsive" and "img-thumbnail". The image has an alt
     # attribute with the contents of alternate_text.
+    %(
+      <img src='#{source}'class='img-responsive img-thumbnail' alt='#{alternate_text}''>
+    )
   end
 
   def bootstrap_alert(message, type='info')
-    # Generates the HTML for a Bootstrap alert (useful since it can be diffcult
-    # to remember all HTML needed to do it right). It should show the message in
-    # the "message" variable and should have a class appropriate for the "type"
+    # Generates the HTML for a Bootstrap alert. It shows the message in
+    # the "message" variable and will have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
+    %(
+      <div class='alert alert-#{type} alert-dismissible' role='alert'>
+        #{message}
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+      </div>
+    )
   end
 
   def current_date_and_time
-    # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
-    # Time.now.strftime is a great method chain you can use for this purpose.
-    # See how to use it at: http://apidock.com/ruby/Time/strftime
+    # Gives the current date and time in the format: "February 6, 2015 at 4:25pm"
+    Time.now.strftime('%B %-e, %Y at %-l:%M%P')
   end
 
 end
